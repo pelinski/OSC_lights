3cdd855af2d4b89efb29f5ff3f56bb39d7ce6142
kriesl <Chris.Retz@me.com>
2019-05-09 23:21:00 +0200

    adjusted to fit the TouchOSC template

--- OSCServer_toArduino.py
+++ OSCServer_toArduino.py
@@ -1,4 +1,4 @@
-
+import types
 import argparse
 import math
 import serial
@@ -7,50 +7,57 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 
 
-def selectProgram_handler(unused_addr, args): 
-    x = args[0]
-    y = args[1]
-    value = args[2]
-    print("Received selectProgram-message:", args[0], args[1], args[2], sep=' ')
-    s.write("p"+ x + y + value )
+def selectProgram_handler(addr, args):
+    addr =  addr[-3:]
+    num = 0
+    if(addr[0]== '1'):
+        num = 5
+    num = num + int(addr[2])
     
-def speedFader_handler(unused_addr, args):  
-    value = args[0]
-    print("Received selectProgram-message:", args[0] sep=' ')
-    s.write("f" + value )                            
+    print("p;"+ str(num)+ ";" + str(int((args))))
+        
+    #print("Received selectProgram-message:", args[0], args[1], args[2])
+    #s.write("p"+ x + y + value )
+    
+def speedFader_handler(addr, args):
+    print("f;"+ str(int((args))))                          
 
-def selectBulbs_handler(unused_addr, args):
-    x = args[0]
-    y = args[1]
-    value = args[2]
-    print("Received selectProgram-message:", args[0], args[1], args[2], sep=' ')
-    s.write("b"+ x + y + value )
+def selectBulbs_handler(addr, args):
+    addr =  addr[-3:]
+    num = 0
+    if(addr[0]== '1'):
+        num = 6
+    num = num + int(addr[2])
+    
+    print("b;"+ str(num)+ ";" + str(int((args))))
 
-def switchManually_handler(unused_addr, args):
-    x = args[0]
-    y = args[1]
-    value = args[2]
-    print("Received selectProgram-message:", args[0], args[1], args[2], sep=' ')
-    s.write("m"+ x + y + value )
+def switchManually_handler(addr, args):
+    addr =  addr[-3:]
+    num = 0
+    if(addr[0]== '1'):
+        num = 6
+    num = num + int(addr[2])
+    
+    print("m;"+ str(num)+ ";" + str(int((args))))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip",
-                        default="127.0.0.1", help="The ip to listen on")
-    parser.add_argument("--port",
-                        type=int, default=5005, help="The port to listen on")
-    args = parser.parse_args()
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("--ip",
+    #                   default="192.168.222.222", help="The ip to listen on")
+    #parser.add_argument("--port",
+    #                    type=int, default=8000, help="The port to listen on")
+    #args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/program/selectMatrix", selectProgram_handler)
+    dispatcher.map("/program/selectMatrix/*/*", selectProgram_handler)
+    dispatcher.map("/selectBulbs/selectMatrix/*/*", selectBulbs_handler)
+    dispatcher.map("/switchManually/selectMatrix/*/*", switchManually_handler)
     dispatcher.map("/program/speedFader", speedFader_handler )
-    dispatcher.map("/selectBulbs/selectMatrix", selectBulbs_handler)
-    dispatcher.map("/switchManually/selectMatrix", switchManually_handler)
-    s = serial.Serial('/dev/ttyAMA0', 9600) # Namen ggf. anpassen
-    s.open()
+    #s = serial.Serial('/dev/ttyAMA0', 9600) # Namen ggf. anpassen
+    #s.open()
 
     server = osc_server.ThreadingOSCUDPServer(
-        (args.ip, args.port), dispatcher)
+        ('192.168.2.112', 5000), dispatcher)
     print("Serving on {}".format(server.server_address))
     server.serve_forever()
